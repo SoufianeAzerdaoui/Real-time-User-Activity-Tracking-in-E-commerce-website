b787ddb5ee68396098163ace1ba542bbb49be449
mustapha <mestaphamansouri@gmail.com>
2024-01-23 14:42:25 +0100

    merge si lfasi

--- src/App.js
+++ src/App.js
@@ -2,19 +2,21 @@ import CategoryBanner from './Categories/CategoryBanner.jsx';
 import Card from './Categories/Cards/Card.jsx';
 import { CategoryProvider } from './Categories/CategoryContext.js';
 import Header from './Header.jsx';
-
+import HandleDailog from './DailogItem/HandleDailog.jsx'
+import MainSection from './Section1/MainSection.jsx'
+import Footer from './Footer/Footer.jsx'
 function App() {
   return (
-    
     <CategoryProvider >
 {/* this changed */}
       <Header />
-
+      <MainSection />
       <CategoryBanner />
-
        <Card />
-
+    <HandleDailog />
+    <Footer />
     </CategoryProvider>
+
   );
 }
 
---
+++ src/DailogItem/HandleDailog.jsx
@@ -0,0 +1,30 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import DailogItem from './DailogItem'
+
+export default function HandleDailog() {
+  const [open, setOpen] = React.useState(false);
+  const [hrt, setHeart] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const info = {
+    title: "Fashion Titanium Steel Colorfast Thick Chain Cuban Chain Necklace-Silver BY ELITE WORLD",
+    category: "Men",
+    old_price: "49.99 Dhs",
+    new_price: "37.49 Dhs",
+    Disponible: "Disponible",
+    desc: "livraison à partir de 9.00 Dhs vers CASABLANCA - Anfa",
+  }
+// {title, category, old_price, new_price, Disponible, desc}
+  return (
+    <React.Fragment>
+      <Button variant="outlined" onClick={handleClickOpen}>
+        Open alert dialog
+      </Button>
+      <DailogItem open={open} hrt={hrt} setHeart={setHeart} setOpen={setOpen} info={info} />
+    </React.Fragment>
+  );
+}
